8e3ee0aafb4ddfe3ebc59cdf1369a9e6d0907f46
belle_meee <adeniranyusufabdullateef@gmail.com>
2017-11-17 13:57:29 +0100

    removed smsAsync

--- nexmo.js
+++ nexmo.js
@@ -16,43 +16,43 @@ nex.prototype.setCredentials = (apiKey, apiSecret) => {
     })
 }
 
+// nex.prototype.sendSms = (from,toPath,message,split= ",") => {
+//     let promise = new Promise((resolve,reject) => {
+//     if(!this.apiKey && !this.apiSecret){
+//         console.log("apiKey and/or apiSecret not set, use .setCredentials(apiKey,apiSecret)");
+//         return
+//     }
+//     // toPath = file absolute path
+//     fs.readFile(toPath, 'utf8', (err,data) => {
+//         if(err){
+//             console.log(err.message)
+//             return;
+//         }
+//         let toArr = data.split(split);
+//         let sentSmsCount = 0;
+//         toArr.forEach((to) => {
+//             this.nexmo.message.sendSms(from, to, message, (err,responseData) => {
+//         if(err){
+//             console.log('err',err.message);
+//             reject(err)
+//             return;
+//         }
+//         else{
+//             sentSmsCount += 1      
+//         }
+//     })
+//         })
+//     let promiseRet = {
+//         "sentSmsCount":sentSmsCount,
+//     }
+//     resolve(promiseRet)
+//     })
+
+// })
+// return promise
+// }
+
 nex.prototype.sendSms = (from,toPath,message,split= ",") => {
-    let promise = new Promise((resolve,reject) => {
-    if(!this.apiKey && !this.apiSecret){
-        console.log("apiKey and/or apiSecret not set, use .setCredentials(apiKey,apiSecret)");
-        return
-    }
-    // toPath = file absolute path
-    fs.readFile(toPath, 'utf8', (err,data) => {
-        if(err){
-            console.log(err.message)
-            return;
-        }
-        let toArr = data.split(split);
-        let sentSmsCount = 0;
-        toArr.forEach((to) => {
-            this.nexmo.message.sendSms(from, to, message, (err,responseData) => {
-        if(err){
-            console.log('err',err.message);
-            reject(err)
-            return;
-        }
-        else{
-            sentSmsCount += 1      
-        }
-    })
-        })
-    let promiseRet = {
-        "sentSmsCount":sentSmsCount,
-    }
-    resolve(promiseRet)
-    })
-
-})
-return promise
-}
-
-nex.prototype.sendSmsSync = (from,toPath,message,split= ",") => {
     if(!this.apiKey && !this.apiSecret){
         console.log("apiKey and/or apiSecret not set, use .setCredentials(apiKey,apiSecret)");
         return
